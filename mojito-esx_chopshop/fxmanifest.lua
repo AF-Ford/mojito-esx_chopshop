7fced40f16105255cd5ed28b44ad86389198417e
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2020-08-02 14:22:44 +0100

    Update fxmanifest.lua

--- mojito-esx_chopshop/fxmanifest.lua
+++ mojito-esx_chopshop/fxmanifest.lua
@@ -2,10 +2,15 @@ fx_version 'adamant'
 
 game 'gta5'
 
+author 'mojito'
+
+description 'chopshop script for fivem'
+
 client_scripts {
    'client/client.lua',
 }
 
 server_scripts {
-	'server/server.lua',
+   'server/server.lua',
+   '@mysql-async/lib/MySQL.lua',
 }
